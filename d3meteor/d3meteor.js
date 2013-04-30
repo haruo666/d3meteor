239705f4236cb13a6e3993f0a107a18c05fd9f6c
haruo666 <haruo666@gmail.com>
2013-04-30 05:15:07 -0400

    Added bootstrap package etc...

--- d3meteor/d3meteor.js
+++ d3meteor/d3meteor.js
@@ -1,19 +1,50 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to d3meteor.";
-  };
+var Texts = new Meteor.Collection('texts');
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
+    
+    Template.textList.texts = function() {
+        // (2) コレクション内の全データを返す
+        return Texts.find();
+    };
+  
+    Template.textList.events({
+        'click #send' : function() {
+            if (typeof console !== 'undefined') {
+                console.log("Send button");
+            }
+            console.log("clicked");
+            var t = $("#text").val();
+            $("#text").val("");
+            
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1; //January is 0!
+            var yyyy = today.getFullYear();
+            var hh = today.getHours();
+            var ss = today.getSeconds();
+            if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
+            today = yyyy + mm + dd + "-" + hh + ":" + ss;
+            
+            Texts.insert({name: '名無しさん', text: t, date: today});
+            return false;
+        } 
+    });
 }
 
 if (Meteor.isServer) {
+    
   Meteor.startup(function () {
     // code to run on server at startup
+                // (3) 全データを消去する
+    Texts.remove({});
+    /*var data = [
+        { name: 'しゅんぺい', text: 'ああ' },
+        { name: 'たえこ', text: 'いい' },
+        { name: 'こうたろう', text: 'うう' },
+        { name: 'ちほ', text: 'ええ' }
+    ];
+    data.forEach(function(txt) {
+        Texts.insert(txt);
+    });*/
   });
 }
