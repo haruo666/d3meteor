963334866717983335f2077f25ba8d99c4e90199
HLabs <haruo666@gmail.com>
2013-05-02 22:39:16 +0900

    テンプレートとか追加。

--- d3meteor/d3meteor.js
+++ d3meteor/d3meteor.js
@@ -2,12 +2,24 @@ var Texts = new Meteor.Collection('texts');
 
 if (Meteor.isClient) {
     
-    Template.textList.texts = function() {
+    Template.chat.texts = function() {
         // (2) コレクション内の全データを返す
         return Texts.find();
     };
+
+    Template.navbar.events({
+        'click #front' : function() {
+            $("#main-content").html(Meteor.render(Template.frontContent));
+        },       
+        'click #twitter' : function() {
+            $("#main-content").html(Meteor.render(Template.twitter));
+        },
+        'click #chat' : function() {
+            $("#main-content").html(Meteor.render(Template.chat));
+        }
+    });
   
-    Template.textList.events({
+    Template.chat.events({
         'click #send' : function() {
             if (typeof console !== 'undefined') {
                 console.log("Send button");
@@ -23,7 +35,7 @@ if (Meteor.isClient) {
             var hh = today.getHours();
             var ss = today.getSeconds();
             if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
-            today = yyyy + mm + dd + "-" + hh + ":" + ss;
+            today = yyyy + "年" + mm + "月" + dd + "日 " + hh + ":" + ss;
             
             Texts.insert({name: '名無しさん', text: t, date: today});
             return false;
